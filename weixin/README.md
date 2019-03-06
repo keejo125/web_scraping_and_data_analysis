33b8f92635774828a777948a584ca0d8208aad70
keejo <keejo@qq.com>
2019-03-06 22:18:33 +0800

    根据数据绘制时间序列图

--- weixin/README.md
+++ weixin/README.md
@@ -8,6 +8,10 @@
     - 3、get_articles()根据get_articles_list生成的csv逐个访问文章固定链接，并导出把保存为一份pdf和一份仅文本的txt。
     - 4、运行`python3 scraping.py`即可，爬取过程会通过`weixin.log`文件记录。
 
+`analysis.py`
+- 使用说明
+    - 1、需先完成信息抓取，有`articles.csv`文件。
+    - 2、运行`python3 analysis.py`即可。
 
 - 坑与建议
     - 1、由于腾讯的反爬做的相当好，建议酌情增加sleep延迟，如果被禁，需要等待较长时间，或更换公众平台账号。
---
+++ weixin/analysis.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# author：zhengk
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+from matplotlib.font_manager import FontProperties
+
+
+def timeline_plot():
+    df_ori = pd.read_csv('articles.csv', sep=';', header=None)
+
+    # 取第一列并分割日期与标题
+    df = df_ori.iloc[:, 0]
+    df = df.str.split(';', expand=True)
+
+    # 格式化日期，设置column，并将日期设置为index
+    df.columns = ['date', 'title']
+    df.date = pd.to_datetime(df.date)
+    df = df.set_index('date')
+
+    # 按月统计文章数"MS"为月初
+    cacu = df.resample("MS").count()
+
+    # 画图
+    fig, ax = plt.subplots(figsize=[20, 5])
+
+    # 线条
+    ax.plot(cacu, 'o-')
+
+    # fig.autofmt_xdate()
+
+    # 通过设置中文字体方式解决中文展示问题
+    font = FontProperties(fname='../font/PingFang.ttc')
+    ax.set_title("新世相文章统计", fontproperties=font)
+    ax.set_xlabel("日期", fontproperties=font)
+    ax.set_ylabel("文章数", fontproperties=font)
+
+    # 设置时间轴
+    formater = mdate.DateFormatter('%Y-%m')
+    ax.xaxis.set_major_formatter(formater)
+    ax.xaxis.set_minor_locator(mdate.MonthLocator())
+    ax.xaxis.set_minor_formatter(mdate.DateFormatter('%m'))
+    ax.xaxis.set_major_locator(mdate.YearLocator())
+    ax.xaxis.set_major_formatter(mdate.DateFormatter('\n\n%Y'))
+    # 显示网格
+    # ax.xaxis.grid(True, which="minor")
+    # ax.yaxis.grid()
+    # 显示数值
+    for a,b in zip(cacu.index, cacu.values):
+        ax.text(a, b, b[0])
+    plt.show()
+
+
+if __name__ == '__main__':
+    timeline_plot()
